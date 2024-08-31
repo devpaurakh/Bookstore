5d1ccafd704c1b9cd409773291b44e67f316c466
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-08-31 11:13:39 +0545

    delete and post api compeleted

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,44 @@
 import express from "express";
 import { conndectDB } from "./config/db.js";
+import Product from "./model/product.model.js";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//create
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    res
+      .status(400)
+      .json({ success: false, message: "Please fill all the fields" });
+    return;
+  }
+  const newPoriduct = new Product(product);
+  try {
+    const createdProduct = await newPoriduct.save();
+    res.status(201).json({
+      success: true,
+      message: "Product successfully created",
+      data: createdProduct,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 });
+
+//delete
+
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
 app.listen(3000, () => {
   conndectDB();
   console.log("http://localhost:3000");
