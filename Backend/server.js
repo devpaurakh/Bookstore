4d173fc1b03cba30b68b2fc5243cc2c243ba74b3
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-08-31 22:35:28 +0545

    crud compeleted with frontend

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 app.use("/api/products", productRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/Frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
   });
